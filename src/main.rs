6831983b2f1aedd0745e1f85a830f6ce589a8c75
Thomas Gale <gale.thomasjames@gmail.com>
2022-05-18 12:38:34 +0100

    Switching to learn wgpu tutorial.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,5 @@
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::Window,
-};
+use learn_wgpu::run;
 
 fn main() {
-    let instances = wgpu::Instance::new(wgpu::Backends::all());
-    for adapter in instances.enumerate_adapters(wgpu::Backends::all()) {
-        println!("{:?}", adapter.get_info())
-    }
-
-    let event_loop = EventLoop::new();
-    let window = Window::new(&event_loop).unwrap();
-    window.set_title("my window");
-    env_logger::init();
-
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
-        }
-    });
+    pollster::block_on(run());
 }
