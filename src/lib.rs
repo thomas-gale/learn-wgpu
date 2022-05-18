a75ff754c7c6827f625d386962d5af9f076aaa4d
Thomas Gale <gale.thomasjames@gmail.com>
2022-05-18 13:34:18 +0100

    Pipeline challenge complete.

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,21 @@ use winit::{
     window::{Window, WindowBuilder},
 };
 
+#[derive(PartialEq)]
+enum ActiveRenderPipeline {
+    Brown,
+    Colour,
+}
+
 struct State {
     surface: wgpu::Surface,
     device: wgpu::Device,
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
-    render_pipeline: wgpu::RenderPipeline,
+    active_render_pipeline: ActiveRenderPipeline,
+    brown_render_pipeline: wgpu::RenderPipeline,
+    colour_render_pipeline: wgpu::RenderPipeline,
 }
 
 impl State {
@@ -69,45 +77,87 @@ impl State {
                 push_constant_ranges: &[],
             });
 
-        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Render Pipeline"),
-            layout: Some(&render_pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &shader,
-                entry_point: "vs_main", // 1.
-                buffers: &[],           // 2.
-            },
-            fragment: Some(wgpu::FragmentState {
-                // 3.
-                module: &shader,
-                entry_point: "fs_main",
-                targets: &[wgpu::ColorTargetState {
-                    // 4.
-                    format: config.format,
-                    blend: Some(wgpu::BlendState::REPLACE),
-                    write_mask: wgpu::ColorWrites::ALL,
-                }],
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList, // 1.
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw, // 2.
-                cull_mode: Some(wgpu::Face::Back),
-                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
-                polygon_mode: wgpu::PolygonMode::Fill,
-                // Requires Features::DEPTH_CLIP_CONTROL
-                unclipped_depth: false,
-                // Requires Features::CONSERVATIVE_RASTERIZATION
-                conservative: false,
-            },
-            depth_stencil: None, // 1.
-            multisample: wgpu::MultisampleState {
-                count: 1,                         // 2.
-                mask: !0,                         // 3.
-                alpha_to_coverage_enabled: false, // 4.
-            },
-            multiview: None, // 5.
-        });
+        let brown_render_pipeline =
+            device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: "vs_main", // 1.
+                    buffers: &[],           // 2.
+                },
+                fragment: Some(wgpu::FragmentState {
+                    // 3.
+                    module: &shader,
+                    entry_point: "fs_brown",
+                    targets: &[wgpu::ColorTargetState {
+                        // 4.
+                        format: config.format,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    }],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw, // 2.
+                    cull_mode: Some(wgpu::Face::Back),
+                    // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    // Requires Features::DEPTH_CLIP_CONTROL
+                    unclipped_depth: false,
+                    // Requires Features::CONSERVATIVE_RASTERIZATION
+                    conservative: false,
+                },
+                depth_stencil: None, // 1.
+                multisample: wgpu::MultisampleState {
+                    count: 1,                         // 2.
+                    mask: !0,                         // 3.
+                    alpha_to_coverage_enabled: false, // 4.
+                },
+                multiview: None, // 5.
+            });
+
+        let colour_render_pipeline =
+            device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: "vs_main", // 1.
+                    buffers: &[],           // 2.
+                },
+                fragment: Some(wgpu::FragmentState {
+                    // 3.
+                    module: &shader,
+                    entry_point: "fs_colour",
+                    targets: &[wgpu::ColorTargetState {
+                        // 4.
+                        format: config.format,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    }],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw, // 2.
+                    cull_mode: Some(wgpu::Face::Back),
+                    // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    // Requires Features::DEPTH_CLIP_CONTROL
+                    unclipped_depth: false,
+                    // Requires Features::CONSERVATIVE_RASTERIZATION
+                    conservative: false,
+                },
+                depth_stencil: None, // 1.
+                multisample: wgpu::MultisampleState {
+                    count: 1,                         // 2.
+                    mask: !0,                         // 3.
+                    alpha_to_coverage_enabled: false, // 4.
+                },
+                multiview: None, // 5.
+            });
 
         Self {
             surface,
@@ -115,7 +165,9 @@ impl State {
             queue,
             config,
             size,
-            render_pipeline,
+            active_render_pipeline: ActiveRenderPipeline::Brown,
+            brown_render_pipeline,
+            colour_render_pipeline,
         }
     }
 
@@ -131,14 +183,29 @@ impl State {
     fn input(&mut self, event: &WindowEvent) -> bool {
         match event {
             WindowEvent::CursorMoved {
-                device_id,
-                position,
+                // device_id,
+                // position,
                 ..
             } => {
-                println!("Cursor moved: {:?} {:?}", device_id, position);
+                // println!("Cursor moved: {:?} {:?}", device_id, position);
+            }
+            WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Space),
+                        ..
+                    },
+                ..
+            } => {
+                if self.active_render_pipeline == ActiveRenderPipeline::Brown {
+                    self.active_render_pipeline = ActiveRenderPipeline::Colour;
+                } else {
+                    self.active_render_pipeline = ActiveRenderPipeline::Brown;
+                }
             }
             _ => {
-                println!("Other event: {:?}", event);
+                // println!("Other event: {:?}", event);
             }
         }
         true
@@ -181,9 +248,15 @@ impl State {
                 depth_stencil_attachment: None,
             });
 
-            // NEW!
-            render_pass.set_pipeline(&self.render_pipeline); // 2.
-            render_pass.draw(0..3, 0..1); // 3.
+            match self.active_render_pipeline {
+                ActiveRenderPipeline::Brown => {
+                    render_pass.set_pipeline(&self.brown_render_pipeline);
+                }
+                ActiveRenderPipeline::Colour => {
+                    render_pass.set_pipeline(&self.colour_render_pipeline);
+                }
+            }
+            render_pass.draw(0..3, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -2,6 +2,7 @@
 
 struct VertexOutput {
     [[builtin(position)]] clip_position: vec4<f32>;
+    [[location(0)]] colour: vec4<f32>;
 };
 
 [[stage(vertex)]]
@@ -12,13 +13,22 @@ fn vs_main(
     let x = f32(1 - i32(in_vertex_index)) * 0.5;
     let y = f32(i32(in_vertex_index & 1u) * 2 - 1) * 0.5;
     out.clip_position = vec4<f32>(x, y, 0.0, 1.0);
+    out.colour = vec4<f32>(x, y, 0.0, 1.0);
     return out;
 }
 
-// Fragment shader
+// Brown Fragment shader
 
 [[stage(fragment)]]
-fn fs_main(in: VertexOutput) -> [[location(0)]] vec4<f32> {
+fn fs_brown(in: VertexOutput) -> [[location(0)]] vec4<f32> {
     return vec4<f32>(0.3, 0.2, 0.1, 1.0);
+}
+
+
+// Position Coloured Fragment shader
+
+[[stage(fragment)]]
+fn fs_colour(in: VertexOutput) -> [[location(0)]] vec4<f32> {
+    return in.colour; 
 }
  
